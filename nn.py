e63b1a37de4cdb0e1f89b50ca6f510b9bde8e270
ShadovvXXI <madmaxmicromax@gmail.com>
2026-02-20 00:32:59 +0300

    feat: add nn architecture

--- nn.py
+++ nn.py
@@ -7,9 +7,22 @@ from torch import nn
 class OsuNeuralNetwork(nn.Module):
     def __init__(self):
         super().__init__()
-        self.seq = nn.Sequential(
-            # TODO : реализация архитектуры нейросети
+        self.conv_pool_part = nn.Sequential(
+            nn.Conv2d(5, 32, 3, 2),
+            nn.Conv2d(32, 64, 3, 2),
+            nn.MaxPool2d(2, 2),
+            nn.MaxPool2d(2, 2)
+        )
+        self.linear_part = nn.Sequential(
+            nn.Linear(8640, 100),
+            nn.Linear(100, 2),
         )
 
     def forward(self, x):
-        return self.seq(x)
+        x = self.conv_pool_part(x)
+        x = nn.Flatten()(x)
+        x = self.linear_part(x)
+        return x
+
+device = torch.accelerator.current_accelerator().type if torch.accelerator.is_available() else "cpu"
+model = OsuNeuralNetwork().to(device)
